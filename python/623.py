2ddaaa319e354c0917e0fcb3f26bf6fbabacfac1
MitchAguilar <madialeja06@gmail.com>
2017-03-11 18:35:14 -0500

    factorial

--- python/623.py
+++ python/623.py
@@ -1,14 +1,14 @@
-x=0
-while True:
-    a=input().split('\n')
-    y=(int)(a[0])
-    if None==a[0]:
-        break
-    b=(int)(1)
-    c=(1)
-    while (b<=y):
-        c*=b
-        b=b+1
-    print(str(y)+'!')
-    print(c)
-print()
+#x=0
+#while True:
+#    a=input().split(' ')
+#    if(a[0]==''):
+#        break
+#    y=(int)(a[0])
+#    b=(int)(1)
+#    c=(1)
+#    while (b<=y):
+#        c*=b
+#        b=b+1
+#    print(str(y)+'!')
+#    print(c)
+#print()
