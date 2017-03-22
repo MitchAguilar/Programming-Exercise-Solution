fb94fd2f49948367941a23a773e24e43d3539076
MitchAguilar <madialeja06@gmail.com>
2017-03-21 21:57:34 -0500

    ostia

--- python/623.py
+++ python/623.py
@@ -1,14 +1,17 @@
 x=0
-while True:
-    a=input().split(' ')
-    if(a[0]==''):
-        break
-    y=(int)(a[0])
-    b=(int)(1)
-    c=(1)
-    while (b<=y):
-        c*=b
-        b=b+1
-    print(str(y)+'!')
-    print(c)
-print()
+try:
+    while True:
+        a=input().split(' ')
+        if(a[0]==''):
+            break
+        y=(int)(a[0])
+        b=(int)(1)
+        c=(1)
+        while (b<=y):
+            c*=b
+            b=b+1
+        print(str(y)+'!')
+        print(c)
+    print()
+except EOFError:
+    pass
