5d90d14335ec3f4224a2f7564ab12ee5afbbee62
MitchAguilar <madialeja06@gmail.com>
2017-04-28 22:18:23 -0500

    10055

--- python/10055.py
+++ python/10055.py
@@ -1,14 +1,8 @@
-a=(int)(input())
-while (a!=0):
-    a-=1
-    e=input().split(' ')
-    b=(int)(e[0])
-    c=(int)(e[1])
-    b=(int)(b/3)
-    c=(int)(c/3)
-    f=(int)(b*c)
-    print(f)
-    
-
-
-
+try:
+    while True:
+        a=input().split(" ")
+        b=int(a[0])
+        c=int(a[1])
+        print(abs(c-b))
+except EOFError:
+    pass
