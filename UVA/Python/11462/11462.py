90c4b3f475d2296a11c20c11ecb5b74284646d80
Maicol Aguilar Perez <ma.aguilar@udla.edu.co>
2018-04-15 00:21:38 -0500

    add new pdf

--- UVA/Python/11462/11462.py
+++ UVA/Python/11462/11462.py
@@ -1,10 +1,20 @@
 while True:
   a=(str)(input())
-  if str(a)=="0":
-    break
+  if str(a)!="0":
+    a = [int(x) for x in input().split()]
+    a.sort()
+    print(" ".join(map(str,a)))
   else:
-    b=input().split(" ")
-    c=list(map(int, b))
-    c=sorted(c)
-    d=list(map(str,c))
-    print(" ".join(d))
+    break
+  
+  
+    #####soluton using bucle for read numbers
+    #a = [int(x) for x in input().split()]
+    #a.sort()
+    #print(" ".join(map(str,a)))
+    
+    #####solution not using bucle
+    #b=input().split(" ")
+    #c=list(map(int, b))
+    #c=sorted(c)
+    #print(" ".join(map(str,c)))
